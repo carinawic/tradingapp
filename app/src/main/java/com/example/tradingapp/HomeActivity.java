373da9a7147beb52c9df9dc06bfdda7f6701a1af
carinawic <carinawi@kth.se>
2020-05-04 13:08:41 +0200

    add picture display

--- app/src/main/java/com/example/tradingapp/HomeActivity.java
+++ app/src/main/java/com/example/tradingapp/HomeActivity.java
@@ -3,15 +3,25 @@ package com.example.tradingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button btnLogout;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,17 @@ public class HomeActivity extends AppCompatActivity {
 
         //find by id from the xml
         btnLogout = findViewById(R.id.btnLogout);
+        imageView = findViewById(R.id.imageView);
+
+        try{
+            URL url = new URL("https://img.icons8.com/material/4ac144/256/twitter.png");
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            imageView.setImageBitmap(bmp);
+
+        } catch (IOException error){
+            error.printStackTrace();
+        }
+
 
         //we add an OnClickListener to the log out-button
         btnLogout.setOnClickListener(new View.OnClickListener(){
